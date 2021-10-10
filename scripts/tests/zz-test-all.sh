3cd0be401863709b97908562d7b0f3664196187f
Joseph Damyanov <yd@itlabs.bg>
2021-10-10 22:43:06 +0300

    Refactor unit tests to use lib-4test.sh
     - Improve lib-4test to better handle corner cases
     - Rewrite unit tests to use latest lib-4test
    
    Task: ya55en/mashmallow-0#46

--- scripts/tests/zz-test-all.sh
+++ scripts/tests/zz-test-all.sh
@@ -8,25 +8,18 @@ echo "$_ta_name_345_: _name_=[$_name_], _ta_name_345_=[$_ta_name_345_]"
 
 print_header() {
     local filename="$1"
-    printf '\n=== Running %s ===\n' $filename
+    printf '\n=== Running %s ===\n' "$filename"
 }
 
 test() {
-    # rc=0
     print_header ./scripts/tests/lib-4test.sh
     ./scripts/tests/lib-4test.sh test
-    # rc=$(expt $rc + $?)
 
     print_header ./scripts/tests/test-small-lib.sh
     ./scripts/tests/test-small-lib.sh
-    # rc=$(expt $rc + $?)
 
     print_header ./scripts/tests/test-create-dot-env.sh
     ./scripts/tests/test-create-dot-env.sh
-    # rc=$(expt $rc + $?)
-
-    printf '\nTOTAL: Tests failed: %u\n' $rc
-    return $rc
 }
 
 if [ "$_name_" = "$_ta_name_345_" ]; then
